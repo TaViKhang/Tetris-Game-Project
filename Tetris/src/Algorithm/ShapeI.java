d9936ef77c9585c6d2c2970e4fc1d2d5e5631fbc
NguyenBaPhucITITIU20278 <91875515+NguyenBaPhucITITIU20278@users.noreply.github.com>
2022-12-24 23:56:08 +0700

    update thuat toan

---
+++ Tetris/src/Algorithm/ShapeI.java
@@ -0,0 +1,16 @@
+package Algorithm.Tetris;
+
+public class ShapeI extends Shape{
+
+	public ShapeI(Controller inGame, int color) {
+		super(inGame, color);
+		maxSize = 3;
+		blocks.get(0).setLocal(3, -1);
+		blocks.get(1).setLocal(4, -1);
+		blocks.get(2).setLocal(5, -1);
+		blocks.get(3).setLocal(6, -1);
+		x = 3;
+		y = -2;
+	}
+
+}
